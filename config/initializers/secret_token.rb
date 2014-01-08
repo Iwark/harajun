5e426ca982115e74e570613bd0430cf01ddf7330
Iwark <iwark02@gmail.com>
2014-01-08 22:24:45 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mito::Application.config.secret_key_base = '066c9f4372d9777f09ee0091e0f2d4f413729c6b3f5e367e8d66a58a29d7752edc4fcf6d9b50a080b771c70efa975b5d45733616a13d0b4e435c7176b62b8a3b'
